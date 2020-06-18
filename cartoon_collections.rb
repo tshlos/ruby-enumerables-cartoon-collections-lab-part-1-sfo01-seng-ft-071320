f90ad20859dcb6d9b207cc72565742e23f110669
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-18 00:24:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each.with_index(1) do |characters, index|
-    puts "#{index}. #{characters}
+    print "#{index}. #{characters}
   end
 end
